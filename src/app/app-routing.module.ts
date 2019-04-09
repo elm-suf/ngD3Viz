bfac4c0c5aa3c33cca362cce2a4c07a761f8208c
elm-suf <elm.sufian@gmail.com>
2019-04-09 13:11:32 +0200

    set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BarchartComponent} from './components/barchart/barchart.component';
+import {TestComponent} from './components/test/test.component';
+import {ScatterPlotComponent} from './components/scatter-plot/scatter-plot.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'barchart', component: BarchartComponent},
+  {path: 'test', component: TestComponent},
+  {path: 'scatter-plot', component: ScatterPlotComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BarchartComponent} from './components/barchart/barchart.component';
+import {HttpClientModule} from '@angular/common/http';
+import {TestComponent} from './components/test/test.component';
+import {ScatterPlotComponent} from './components/scatter-plot/scatter-plot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent,
+    TestComponent,
+    ScatterPlotComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
